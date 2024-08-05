fa2ff7fb9bbbe93eb23d4ea78769d4755e862384
Divya <its.divyamantri@gmail.com>
2024-08-05 11:41:16 +0530

    Add Print 111-2 Program

---
+++ Day-5_[Print_111-2].cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    //Declared & Initilize the variable with last number 
+    int last_number=111;
+
+    //Iterate the loop backward from 111 to 2
+    for(int i=111;i>=2;i--)
+    {
+        cout<<i<<" ";
+    }
+}
