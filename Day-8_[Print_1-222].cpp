0653c2aa222f8ab2ecf28fece263f0eaa7452f94
Divya <its.divyamantri@gmail.com>
2024-08-08 18:19:03 +0530

    Add Print 1-222 Program

---
+++ Day-8_[Print_1-222].cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    //initialised variable
+    int num=1;
+    cout<<"Number from 1 to 222 are::"<<endl;
+    //iterate the loop 
+    for(int i=1;i<=222;i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}   
