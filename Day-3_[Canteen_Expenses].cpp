ac019b9ad11db11307b86276c8ce72100fdd61a4
Divya <its.divyamantri@gmail.com>
2024-08-09 19:53:36 +0530

    Updated input validation logic to prevent users from entering float values

--- Day-3_[Canteen_Expenses].cpp
+++ Day-3_[Canteen_Expenses].cpp
@@ -8,6 +8,14 @@ int main()
     int total_items;
     cout<<"Total number of items are :";
     cin>>total_items;
+    while(cin.fail() || cin.peek()!='\n')
+    {
+        cin.clear();
+        cin.ignore(INT32_MAX,'\n');
+
+        cout<<"Invalid number!.....Please enter a valid integer:";
+        cin>>total_items;
+    }
 
     int sum=0;
     int price;
