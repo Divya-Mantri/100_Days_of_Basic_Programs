17f86bcc467eca4b41a444875da1b8386d39525f
Divya <its.divyamantri@gmail.com>
2024-08-10 13:16:49 +0530

    Added Printing a String twice Program

---
+++ Day-10_[Print_String_Twice].cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    // Initialized a string variable
+    string s;
+    //Prompt to the user
+    cout<<"Enter the String you want to print twice:";
+    //Take input in the variable
+    cin>>s;
+    //Displayed on the screen 
+    cout<<s<<'\n'<<s<<endl;
+
+}
