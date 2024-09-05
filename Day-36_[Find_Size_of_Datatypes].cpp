5ba89b140163b65a2d2cbd602f05d1981f4ee45e
Divya <its.divyamantri@gmail.com>
2024-09-05 22:02:52 +0530

    Added a program to find size of datatypes

---
+++ Day-36_[Find_Size_of_Datatypes].cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+int main() 
+{
+    cout<<"The size of (char) datatype is :: "<<sizeof(char)<<" byte"<<endl;
+    cout<<"The size of (int) datatype  is :: "<<sizeof(int)<<" byte"<<endl;
+    cout<<"The size of (long) datatype is ::"<<sizeof(long)<<" byte"<<endl;
+    cout<<"The size of (long long) datatype is ::"<<sizeof(long long)<<" byte"<<endl;
+    cout<<"The size of (float) datatype is ::"<<sizeof(float)<<" byte"<<endl;
+    cout<<"The size of (double) datatype is ::"<<sizeof(double)<<" byte"<<endl;
+    cout<<"The size of (bool) datatype is ::"<<sizeof(bool)<<" byte"<<endl;
+
+
+    return 0;
+
+}
