59fa847d3ee07f73894080215afde533708198df
Divya <its.divyamantri@gmail.com>
2024-09-06 19:25:08 +0530

    Added a program to print max & min limit of datatypes

---
+++ Day-37_[Range_of_datatypes].cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<climits>
+#include<cfloat>
+using namespace std;
+ 
+int main()
+{
+    cout<<"The maximum limit of int data type is :: "<<INT32_MAX<<endl;
+    cout<<"The minimum limit of int data type is :: "<<INT32_MIN<<endl;
+    cout<<"The maximum limit of long long data type is :: "<<INT64_MAX<<endl;
+    cout<<"The minimum limit of long long data type is :: "<<INT64_MIN<<endl;
+    cout<<"The maximum limit of char data type is :: "<<CHAR_MAX<<endl;
+    cout<<"The minimum limit of char data type is :: "<<CHAR_MIN<<endl;
+    cout<<"The maximum limit of float data type is :: "<<FLT_MAX<<endl;
+    cout<<"The minimum limit of float data type is :: "<<FLT_MIN<<endl;
+    cout<<"The maximum limit of double data type is :: "<<DBL_MAX<<endl;
+    cout<<"The minimum limit of double data type is :: "<<DBL_MIN<<endl;
+}
