f3b6b3cc03b7c5d52a758df04b11c6ff0d9475ec
Divya <its.divyamantri@gmail.com>
2024-09-19 20:18:33 +0530

    Calculating Entrance fee according to number of people

---
+++ Day-50_[Calculate_Park_Entrance_Fee].cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int Entrance_Fee=30;
+
+    
+    int No_Of_Person;
+    cout<<"Enter No. of Person to take Entry :: ";
+    cin>>No_Of_Person;
+
+    cout<<"Your Total Park Entrance Fee :: "<<No_Of_Person*Entrance_Fee<<endl;
+
+    return 0;
+}
