f800537b17c6077e459e1d4e9312777dd7be04d5
Divya <its.divyamantri@gmail.com>
2024-10-05 21:57:07 +0530

    Added a program to print Reverse Alphabet Traingle Pattern

---
+++ Day-66_[Reverse_Alphabet_Triangle_Pattern].cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-(i+1);j>=0;j--)
+        {
+            char c='A'+j;
+            cout<<c<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
