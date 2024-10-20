4372e71dc08df467ea8ef0ac82d4403581ae3d70
Divya <its.divyamantri@gmail.com>
2024-10-20 21:05:10 +0530

    Added a program to print given numbers

---
+++ Day-81_[Print_digits_XandY].cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//Program to print digits X and Y  
+int main()
+{
+    int X;
+    cout<<"Enter the number for X : ";
+    cin>> X;
+
+    int Y;
+    cout<<"Enter the number for Y : ";
+    cin>>Y;
+
+    cout<<"Given numbers are : "<<X<<" "<<Y<<endl;
+
+
+    return 0;
+}
