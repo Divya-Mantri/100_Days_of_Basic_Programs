e9a695a364ae1a1784f208fd40271a21b37dd48b
Divya <its.divyamantri@gmail.com>
2024-10-30 21:01:59 +0530

    Added a program to print predecessor and successor of the given X

---
+++ Day-91_[Predecessor_N_Successor_of_X].cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//Program for printing predecessor and successor of given X
+int main()
+{
+    int X;
+    cout<<"Enter the value of X : ";
+    cin>>X;
+
+    cout<<"The Predecessor of X is : "<<X-1<<"\n"<<"The Successor of X is : "<<X+1<<endl;
+
+    return 0;
+
+}
